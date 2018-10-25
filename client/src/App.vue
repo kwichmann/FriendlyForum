3e5385764ed7c4a2c32fde51968efbfd4ccacf1c
kwichmann <kristian.wichmann@gmail.com>
2018-10-26 01:14:26 +0200

    Vue client and basic landing page set up

---
+++ client/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <app-nav></app-nav>
+    </div>
+    <router-view class='container'/>
+  </div>
+</template>
+
+<script>
+import AppNav from '@/components/AppNav.vue';
+
+export default {
+  components: {
+    AppNav
+  },
+}
+</script>
+
+
+<style lang="scss">
+</style>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <div class="jumbotron">
+      <h1 class="display-3">Welcome to the forum, friend.</h1>
+      <p class="lead">A coding project by kwichmann.</p>
+      <hr class="my-4">
+      <p>A place to share your thoughts and ideas.</p>
+      <p class="lead">
+        <a class="btn btn-primary btn-lg" href="#" role="button">Sign up!</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+};
+</script>
